640770d9ab79261e752978643b7bf62fc3a11b22
Nelson Li <nelsonli1015@gmail.com>
2026-06-15 20:20:22 -0700

    Add: valid-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-1.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isValidBST(root) {
+        let curr = null;
+        const dfs = (node) => { // curr = 5
+            if (!node) return true;
+            const left = dfs(node.left);
+            console.log(node.val, curr)
+            if (curr === null || node.val > curr) curr = node.val
+            else return false;
+            const right = dfs(node.right)
+            return left && right;
+        }
+        return dfs(root)
+    }
+}
+
+
+/***
+ * 
+ *         5
+ *        / \
+ *       4    6
+ *      /\   / \
+ *     x  x 3   7
+ * 
+ * 
+ * 
+ */
